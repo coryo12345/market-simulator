16db4baabf31626e3ac2f8be06c689c3bae4594b
coryo12345 <corydio12@gmail.com>
2024-04-01 22:40:45 -0400

    show error if fails to load

--- src/components/app/SaveState.vue
+++ src/components/app/SaveState.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useMarketStore } from '../../stores/market';
 import { useWalletStore } from '../../stores/wallet';
 import { SaveState } from '../../utils/SaveState';
+
+const showError = ref(false);
 
 function save() {
   const walletStore = useWalletStore();
@@ -15,10 +18,22 @@ function load() {
   const marketStore = useMarketStore();
   const saveState = new SaveState(walletStore, marketStore);
   const success = saveState.loadFromLocalStorage();
-  // TODO handle error
+  if (!success) {
+    showError.value = true;
+  }
 }
 </script>
 
 <template>
-  <slot name="activator" :save="save" :load="load"></slot>
+  <v-snackbar v-model="showError" color="red">
+    <template #activator>
+      <slot name="activator" :save="save" :load="load"></slot>
+    </template>
+    <div class="d-flex align-center">
+      <v-icon icon="mdi-alert-circle-outline" class="mr-1 mb-1" />
+      <span>
+        Unable to load save data. Save content may be corrupted or missing.
+      </span>
+    </div>
+  </v-snackbar>
 </template>
